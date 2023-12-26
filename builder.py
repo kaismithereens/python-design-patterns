7ee5d1054ba59b80f11cebc0aa83442d4885b3e9
Kai <kai.nevermind@gmail.com>
2023-12-26 09:46:21 +0100

    Builder pattern

--- builder.py
+++ builder.py
@@ -4,10 +4,13 @@ class Director():
         self._builder = builder 
         
     def construct_car(self):
-        
+        self._builder.create_new_car()
+        self._builder.add_model()
+        self._builder.add_tires()
+        self._builder.add_engine()
         
     def get_car(self):
-        
+        return self._builder.car
         
         
  
@@ -30,8 +33,8 @@ class SkyLarkBuilder(Builder):
     def add_tires(self):
         self.car.tires = "Regular tires"
 
-    
-
+    def add_engine(self):    
+        self.car.engine = "Turbo engine"
 
 class Car():
     """Product"""
@@ -43,4 +46,8 @@ class Car():
     def __str__(self):
         return '{} | {} | {}'.format(self.model, self.tires, self.engine)
 
-
+builder = SkyLarkBuilder()
+director = Director(builder)
+director.construct_car()
+car = director.get_car()
+print(car)
